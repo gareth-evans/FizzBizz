9ce23de80ce3eceb4898e54630eddc3c6e3b2609
Gareth Evans <gareth@garethevans.com>
2015-06-29 13:46:39 +0100

    Added word occurences count to output

--- FizzBuzz.Tests/FormattingFizzBuzzSequence.cs
+++ FizzBuzz.Tests/FormattingFizzBuzzSequence.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Text;
+using NUnit.Framework;
 
 namespace FizzBuzz.Tests
 {
@@ -6,15 +7,26 @@ namespace FizzBuzz.Tests
     public class FormattingFizzBuzzSequence
     {
         [Test]
-        public void ShouldProduceSpaceDelimeteredList()
+        public void ShouldGenerateSummaryReportForFizzBuzz()
         {
-            const string expectedResult = "1 2 lucky";
-            var sequence = new object[] {1, 2, "lucky"};
+            var sequence = new object[]
+            {
+                1, 2, "lucky", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", "lucky", 14, "fizzbuzz", 16, 17, "fizz", 19, "buzz"
+            };
+
+            var expected = new StringBuilder("1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz")
+                .AppendLine("fizz: 4")
+                .AppendLine("buzz: 3")
+                .AppendLine("fizzbuzz: 1")
+                .AppendLine("lucky: 2")
+                .Append("integer: 10")
+                .ToString();
+
             var sut = new FizzBuzzFormatter();
 
             var result = sut.FormatFizzBuzzSequence(sequence);
 
-            Assert.That(result, Is.EqualTo(expectedResult));
+            Assert.That(result, Is.EqualTo(expected));
         }
     }
 }
---
+++ FizzBuzz.Tests/OutputtingFizzBuzzSequence.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using AutoMoq;
+using NUnit.Framework;
+
+namespace FizzBuzz.Tests
+{
+    [TestFixture]
+    public class OutputtingFizzBuzzSequence
+    {
+        [Test]
+        public void ShouldWriteSequence()
+        {
+            const string sequenceString = "SomeString";
+            var sequence = new object[] {1, 2, 3};
+
+            var mocker = new AutoMoqer();
+            var sut = mocker.Create<FizzBuzzMediator>();
+
+            mocker.GetMock<IFizzBuzzSequenceGenerator>()
+                .Setup(x => x.GenerateFizzBuzzSequence(1, 3))
+                .Returns(sequence);
+
+            mocker.GetMock<IFizzBuzzFormatter>()
+                .Setup(x => x.FormatFizzBuzzSequence(sequence))
+                .Returns(sequenceString);
+
+            sut.GenerateFizzBuzzSequence(1,3);
+
+            mocker.GetMock<TextWriter>()
+                  .Verify(x => x.WriteLine(sequenceString));
+        }
+    }
+}
